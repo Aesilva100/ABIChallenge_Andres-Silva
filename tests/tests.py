d7dd10c41cd369345f5eaa0a83bf22e701f262a7
aesilva100 <ae.silva10@uniandes.edu.co>
2024-04-04 11:07:21 -0500

    tests fix

--- tests/tests.py
+++ tests/tests.py
@@ -1,24 +1,26 @@
 import unittest, joblib, os
+
 class TestModeloIris(unittest.TestCase):
 
-    model_path = os.path.join(os.path.dirname(__file__), '..', 'model', 'gaussian_nb_model.pkl')
-    model = joblib.load(model_path)
+    def setUp(self):
+        # Aquí, se establece el modelo como una variable de instancia usando 'self.model'
+        model_path = os.path.join(os.path.dirname(__file__), '..', 'model', 'gaussian_nb_model.pkl')
+        self.model = joblib.load(model_path)
 
     def test_prediccion_individual(self):
-        # Supongamos que tu función de predicción espera una lista de características
-        # Cambia los valores de prueba según tu modelo específico
+        # Se utiliza 'self.model' para acceder al modelo cargado
         datos_de_prueba = [5.1, 3.5, 1.4, 0.2]
-        resultado = model([datos_de_prueba])  # Envía los datos como un lote de un solo elemento
-        self.assertEqual(len(resultado), 1)  # Asegura que el resultado sea una sola predicción
+        resultado = self.model.predict([datos_de_prueba])  # Se cambia 'model' por 'self.model'
+        self.assertEqual(len(resultado), 1)  # Verifica que el resultado sea una sola predicción
 
     def test_prediccion_por_lote(self):
-        # Supongamos que tu función de predicción puede manejar varios conjuntos de características
+        # Se utiliza 'self.model' para acceder al modelo cargado
         datos_de_prueba = [
             [5.1, 3.5, 1.4, 0.2],
             [6.2, 3.4, 5.4, 2.3]
         ]
-        resultados = model(datos_de_prueba)
-        self.assertEqual(len(resultados), 2)  # Asegura que haya una predicción por cada conjunto de características
+        resultados = self.model.predict(datos_de_prueba)  # Se cambia 'model' por 'self.model'
+        self.assertEqual(len(resultados), 2)  # Verifica que haya una predicción por cada conjunto de características
 
 if __name__ == '__main__':
     unittest.main()
